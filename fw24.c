a6dbf1701d904ad2b476320abcf53788a282157c
PaavanShah123 <work.paavan.shah@gmail.com>
2026-09-07 22:41:18 +0530

    fw24.c

---
+++ fw24.c
@@ -0,0 +1,12 @@
+    int num, digit, rev = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        digit = num % 10;
+        rev = rev * 10 + digit;
+        num = num / 10;
+    }
+
+    printf("Reversed number: %d\n", rev);
